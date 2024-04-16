2ec47399243ab7419869a738af5b1026916261ea
Cristoffer Pogan <cristoffer_pogan@econeteditora.com.br>
2024-04-16 13:55:57 -0300

    [UPDATE] Go: desenvolvendo uma API REST (04.Criando, deletando e editando com Gorm)

--- api-go-rest/controllers/controllers.go
+++ api-go-rest/controllers/controllers.go
@@ -28,3 +28,28 @@ func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&p, id)
 	json.NewEncoder(w).Encode(p)
 }
+
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+    var p models.Personalidade
+    json.NewDecoder(r.Body).Decode(&p)
+    database.DB.Create(&p)
+    json.NewEncoder(w).Encode(p)
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id := vars["id"]
+    var p models.Personalidade
+    database.DB.Delete(&p, id)
+    json.NewEncoder(w).Encode(p)
+}
+
+func EditaPersonalidade(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id := vars["id"]
+    var p models.Personalidade
+    database.DB.First(&p, id)
+    json.NewDecoder(r.Body).Decode(&p)
+    database.DB.Save(&p)
+    json.NewEncoder(w).Encode(p)
+}
