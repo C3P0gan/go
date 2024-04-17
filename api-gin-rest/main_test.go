a1b8210f9fabea9e0d9fe0f9108e9da953cc7757
Cristoffer Pogan <cristoffer_pogan@econeteditora.com.br>
2024-04-17 13:08:31 -0300

    [UPDATE] Go: validações, testes e páginas HTML (02.Testes)

---
+++ api-gin-rest/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"api-gin-rest/controllers"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func SetupDasRotasDeTeste() *gin.Engine {
+	rotas := gin.Default()
+	return rotas
+}
+
+func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saudacao)
+	req, _ := http.NewRequest("GET", "/cris", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+	mockDaResposta := `{"API diz:":"E ai cris, tudo beleza?"}`
+	respostaBody, _ := io.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
--- api-gin-rest/routes/routes.go
+++ api-gin-rest/routes/routes.go
@@ -9,11 +9,11 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
-    r.GET("/:nome", controllers.Saudacao)
-    r.POST("/alunos", controllers.CriaNovoAluno)
-    r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
-    r.DELETE("/alunos/:id", controllers.DeletaAluno)
-    r.PATCH("/alunos/:id", controllers.EditaAluno)
-    r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	r.GET("/:nome", controllers.Saudacao)
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.Run()
 }
