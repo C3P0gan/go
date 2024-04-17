338c6a531ee8d519860761471e68bb994abbf5e8
Cristoffer Pogan <cristoffer_pogan@econeteditora.com.br>
2024-04-17 15:11:21 -0300

    [UPDATE] Go: validações, testes e páginas HTML (04.Aprofundando em testes)

--- api-gin-rest/main_test.go
+++ api-gin-rest/main_test.go
@@ -83,9 +83,9 @@ func TestBuscaAlunoPorIDHandler(t *testing.T) {
 	r.ServeHTTP(resposta, req)
 	var alunoMock models.Aluno
 	json.Unmarshal(resposta.Body.Bytes(), &alunoMock)
-    assert.Equal(t, "Nome do Aluno Teste", alunoMock.Nome)
-    assert.Equal(t, "12345678901", alunoMock.CPF)
-    assert.Equal(t, "123456789", alunoMock.RG)
+	assert.Equal(t, "Nome do Aluno Teste", alunoMock.Nome)
+	assert.Equal(t, "12345678901", alunoMock.CPF)
+	assert.Equal(t, "123456789", alunoMock.RG)
 	assert.Equal(t, http.StatusOK, resposta.Code)
 }
 
@@ -94,29 +94,29 @@ func TestDeletaAlunoHandler(t *testing.T) {
 	CriaAlunoMock()
 	r := SetupDasRotasDeTeste()
 	r.DELETE("/alunos/:id", controllers.DeletaAluno)
-    pathDeBusca := "/alunos/" + strconv.Itoa(ID) 
-    req, _ := http.NewRequest("DELETE", pathDeBusca, nil)
-    resposta := httptest.NewRecorder()
-    r.ServeHTTP(resposta, req)
-    assert.Equal(t, http.StatusOK, resposta.Code)
+	pathDeBusca := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("DELETE", pathDeBusca, nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
 
 func TestEditaAlunoHandler(t *testing.T) {
-    database.ConectaComBancoDeDados()
-    CriaAlunoMock()
-    defer DeletaAlunoMock()
-    r := SetupDasRotasDeTeste()
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
 	r.PATCH("/alunos/:id", controllers.EditaAluno)
-    aluno := models.Aluno{Nome: "Nome do Aluno Teste", CPF: "47123456789", RG: "123456700"}
-    valorJson, _ := json.Marshal(aluno)
-    pathParaEditar := "/alunos/" + strconv.Itoa(ID)
-    req, _ := http.NewRequest("PATCH", pathParaEditar, bytes.NewBuffer(valorJson))
-    resposta := httptest.NewRecorder()
-    r.ServeHTTP(resposta, req)
-    var alunoMockAtualizado models.Aluno
-    json.Unmarshal(resposta.Body.Bytes(), &alunoMockAtualizado)
-    assert.Equal(t, "47123456789", alunoMockAtualizado.CPF) 
-    assert.Equal(t, "123456700", alunoMockAtualizado.RG) 
-    assert.Equal(t, "Nome do Aluno Teste", alunoMockAtualizado.Nome) 
+	aluno := models.Aluno{Nome: "Nome do Aluno Teste", CPF: "47123456789", RG: "123456700"}
+	valorJson, _ := json.Marshal(aluno)
+	pathParaEditar := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("PATCH", pathParaEditar, bytes.NewBuffer(valorJson))
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	var alunoMockAtualizado models.Aluno
+	json.Unmarshal(resposta.Body.Bytes(), &alunoMockAtualizado)
+	assert.Equal(t, "47123456789", alunoMockAtualizado.CPF)
+	assert.Equal(t, "123456700", alunoMockAtualizado.RG)
+	assert.Equal(t, "Nome do Aluno Teste", alunoMockAtualizado.Nome)
 	assert.Equal(t, http.StatusOK, resposta.Code)
 }
